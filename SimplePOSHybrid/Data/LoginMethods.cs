1d4910a7e34c824e378ced631d43b7c14b95cfa1
dhvihan <danulvihan@gmail.com>
2023-01-26 17:14:24 +0530

    backend methods craeting

---
+++ SimplePOSHybrid/Data/LoginMethods.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+//using NavigationManager Navigation
+
+namespace SimplePOSHybrid.Data
+{
+    public class LoginMethods
+    {
+
+        public void LoginResponse()
+        {
+
+
+            //Navigation.NavigateTo("/home");
+        }
+    }
+}
