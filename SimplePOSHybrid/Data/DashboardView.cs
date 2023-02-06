788940785a61a521dbdaf54171efb053a5dac63c
dhvihan <danulvihan@gmail.com>
2023-02-06 12:05:23 +0530

    got menuitemlist items from the backend using a separate method

--- SimplePOSHybrid/Data/DashboardView.cs
+++ SimplePOSHybrid/Data/DashboardView.cs
@@ -4,9 +4,11 @@ using RestSharp;
 using SimplePOSHybrid.Models.PartnerMenu;
 using System.Collections.ObjectModel;
 using System.Net;
+using static MudBlazor.CategoryTypes;
 
 namespace SimplePOSHybrid.Data
 {
+
     public partial class DashboardView : ObservableObject
     {
 
@@ -21,18 +23,18 @@ namespace SimplePOSHybrid.Data
 
         private readonly string apitoken = "5b9f7f95a73c1b8270ef7cbe664324aac2e9f981f906ad366c64f2b107c90be7";
 
-        string responseContent = string.Empty;
+
         public DashboardView() { }
 
-        public async Task<Rootobject> LoadResponse()
+
+        public RestRequest Res()
         {
             GlobalUsings link = new();
             var client = new RestClient();
 
-
-
             try
             {
+
                 GetPartnerItemListToUploadReqModel reqmodel = new()
                 {
                     RequestId = "7ee53650-37b8-464c-90e9-85d89f8ab12a",
@@ -55,10 +57,7 @@ namespace SimplePOSHybrid.Data
                 {
                     CompanyId = 51
                 };
-
                 reqmodel.company = co;
-
-
 
 
                 var request = new RestRequest(link.apilinkpub + "api/PartnerMenu/GetPartnerItemListToUpload").AddJsonBody(reqmodel);
@@ -69,7 +68,26 @@ namespace SimplePOSHybrid.Data
                 request.AddHeader("Content-Type", "application/json");
                 request.AddHeader("TImeStamp", "125635753");
 
-                RestResponse response = await client.PostAsync(request);
+                return request;
+
+            }
+            catch (Exception ex)
+            {
+                return new RestRequest();
+            }
+
+        }
+        //Configuration
+        public async Task<Rootobject> LoadResponse()
+        {
+
+            var client = new RestClient();
+
+
+            try
+            {
+
+                RestResponse response = await client.PostAsync(Res());
 
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
@@ -78,7 +96,8 @@ namespace SimplePOSHybrid.Data
                     Console.WriteLine(responseContent);
 
                     Rootobject array = JsonConvert.DeserializeObject<Rootobject>(responseContent);
-
+                    //Responsedata a = array.ResponseData;
+                    //List l = a.MenuItemList.ToList();
                     return array;
                     //LItems = new ObservableCollection<Rootobject>((IEnumerable<Rootobject>)array);
                     //Console.WriteLine(LItems);
@@ -100,8 +119,8 @@ namespace SimplePOSHybrid.Data
 
         }
 
-
-        public static string[] insertCat(Rootobject te)
+        //Filtering the categories
+        public static string[] InsertCat(Rootobject te)
         {
             int n = te.ResponseData.MenuItemList.Length;
             string[] cate = new string[n];
@@ -163,7 +182,51 @@ namespace SimplePOSHybrid.Data
             return arr;
         }
 
-        
+
+        //getting all the items to each categories.
+        public async void GetItems()
+        {
+
+            var client = new RestClient();
+
+
+            try
+            {
+
+                RestResponse response = await client.PostAsync(Res());
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var responseContent = response.Content.ToString();
+
+                    Console.WriteLine(responseContent);
+
+
+
+                    Rootobject array = JsonConvert.DeserializeObject<Rootobject>(responseContent);
+                    var a = array.ResponseData.MenuItemList;
+                    Console.WriteLine(a);
+                    //List l = a.MenuItemList.ToList();
+                    //List<Rootobject> array1 = JsonConvert.DeserializeObject<List<Rootobject>>(a);
+
+                    //LItems = new ObservableCollection<Rootobject>(array1);
+                    //Console.WriteLine(LItems);
+                }
+
+                else
+                {
+                    Console.WriteLine(response.StatusCode);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+
+
+            }
+
+        }
 
 
     }
