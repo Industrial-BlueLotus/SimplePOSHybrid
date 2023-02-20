eb6d4dd5307163dc1429e4aaa9a7c9af7066ae40
dhvihan <danulvihan@gmail.com>
2023-02-20 12:11:52 +0530

    not stages. related to the previous commit

--- SimplePOSHybrid/Data/OrderStateService.cs
+++ SimplePOSHybrid/Data/OrderStateService.cs
@@ -4,14 +4,17 @@ namespace SimplePOSHybrid.Data
 {
     public class OrderStateService
     {
+        private IEnumerable<Menuitemlist> Elements = new List<Menuitemlist>();
 
-        public void orderState(Menuitemlist item)
+        public IEnumerable<Menuitemlist> ElementMthd(List<Menuitemlist> orderitems)
         {
-            orderitems.Add(item);
-            Console.WriteLine(orderitems);
+            Elements = orderitems.AsEnumerable();
+            return Elements;
+        }
 
-
-
+        public IEnumerable<Menuitemlist> GetElements()
+        {
+            return Elements;
         }
     }
 }
