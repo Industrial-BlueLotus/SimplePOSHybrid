87726b3043524edc1d765d637133e1ad7bf245b4
dhvihan <danulvihan@gmail.com>
2023-04-04 14:56:28 +0530

    got the response. state management working for username and usercompany

--- SimplePOSHybrid/Data/LoginStateService.cs
+++ SimplePOSHybrid/Data/LoginStateService.cs
@@ -1,22 +1,71 @@
-﻿using SimplePOSHybrid.Models;
+﻿using SimplePOSHybrid.Models.Login;
+using SimplePOSHybrid.Models.UserCompanies;
 using RestSharp;
 using Newtonsoft.Json;
+using static MudBlazor.CategoryTypes;
+using System.Net;
 
 namespace SimplePOSHybrid.Data
 {
     public class LoginStateService
     {
-        private LoginResModel ResModel = new();
+        private NewLoginResModel ResModel = new();
 
-        public LoginResModel LoginStateMethod(RestResponse response)
+        private UserCompaniesModel usrcompmdl = new();
+
+        GlobalUsings link = new();
+
+        public async Task<UserCompaniesModel> getUserCompanies(string apitoken)
+        {
+            var client = new RestClient();
+
+            string responseContent = string.Empty;
+
+
+            try
+            {
+                var request = new RestRequest(link.apilinkpub + "api/Authentication/getUserCompanies");
+                request.Method = Method.Post;
+
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", "Bearer " + apitoken);
+                request.AddHeader("IntegrationID", "7ee53650-37b8-464c-90e9-85d89f8ab12a");
+
+                RestResponse response = await client.PostAsync(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    responseContent = response.Content.ToString();
+                    Console.WriteLine(responseContent);
+
+                    List<UserCompaniesModel> arr = JsonConvert.DeserializeObject<List<UserCompaniesModel>>(responseContent);
+                    usrcompmdl.companyName = arr[0].companyName;
+                    return usrcompmdl;
+
+                }
+                return new UserCompaniesModel();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                return new UserCompaniesModel();
+
+                //await _navigation.PushAsync(new AlertPop());
+
+            }
+
+        }
+
+        public async Task<NewLoginResModel> LoginStateMethodAsync(RestResponse response)
         {
             string responseContent = response.Content.ToString();
-            LoginResModel array = JsonConvert.DeserializeObject<LoginResModel>(responseContent);
+            NewLoginResModel array = JsonConvert.DeserializeObject<NewLoginResModel>(responseContent);
 
-            ResModel.ResponseData.User.UserId = array.ResponseData.User.UserId;
-            ResModel.ResponseData.User.UserCompanies = array.ResponseData.User.UserCompanies;
-            Console.WriteLine(ResModel.ResponseData.User.UserId);
-
+            ResModel.username = array.username;
+            await getUserCompanies(array.token);
 
             return ResModel;
         }
@@ -26,12 +75,45 @@ namespace SimplePOSHybrid.Data
 
         public string GetUserName()
         {
-            return ResModel.ResponseData.User.UserId;
+            return ResModel.username;
         }
 
-        public Usercompany GetUserCompany()
+        public string GetUserCompany()
         {
-            return ResModel.ResponseData.User.UserCompanies[0];
+            return usrcompmdl.companyName;
         }
     }
+
+
+
+    //public class LoginStateService
+    //{
+    //    private LoginResModel ResModel = new();
+
+    //    public LoginResModel LoginStateMethod(RestResponse response)
+    //    {
+    //        string responseContent = response.Content.ToString();
+    //        LoginResModel array = JsonConvert.DeserializeObject<LoginResModel>(responseContent);
+
+    //        ResModel.ResponseData.User.UserId = array.ResponseData.User.UserId;
+    //        ResModel.ResponseData.User.UserCompanies = array.ResponseData.User.UserCompanies;
+    //        Console.WriteLine(ResModel.ResponseData.User.UserId);
+
+
+    //        return ResModel;
+    //    }
+
+
+    //    public LoginStateService() { }
+
+    //    public string GetUserName()
+    //    {
+    //        return ResModel.ResponseData.User.UserId;
+    //    }
+
+    //    public Usercompany GetUserCompany()
+    //    {
+    //        return ResModel.ResponseData.User.UserCompanies[0];
+    //    }
+    //}
 }
---
+++ SimplePOSHybrid/Models/UserCompanies/UserCompaniesModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePOSHybrid.Models.UserCompanies
+{
+    public class UserCompaniesModel
+    {
+        public int companyKey { get; set; }
+        public string companyName { get; set; }
+        public string companyCode { get; set; }
+        public int isActive { get; set; }
+        public int isApproved { get; set; }
+        public bool isRecordLocked { get; set; }
+        public bool isPersisted { get; set; }
+        public bool isDirty { get; set; }
+        public DateTime createdDate { get; set; }
+        public DateTime updatedDate { get; set; }
+        public DateTime effectiveDate { get; set; }
+        public Addtionaldata addtionalData { get; set; }
+        public bool isDefault { get; set; }
+        public int requestingObjectKey { get; set; }
+
+        public UserCompaniesModel()
+        {
+            addtionalData = new Addtionaldata();
+        }
+    }
+
+    public class Addtionaldata
+    {
+
+    }
+}
+
+
+
