81e6a7a7b06dab6d2854a8886a3f1bbc4d5e3de0
dhvihan <danulvihan@gmail.com>
2023-04-04 17:12:39 +0530

    tring to get categories.

--- SimplePOSHybrid/Data/DashboardView.cs
+++ SimplePOSHybrid/Data/DashboardView.cs
@@ -10,16 +10,16 @@ using System.Net;
 namespace SimplePOSHybrid.Data
 {
 
-    public partial class DashboardView : ObservableObject
+    public class DashboardView
     {
+        private string apitoken;
 
-        [ObservableProperty]
-        ObservableCollection<ItemModel> _LItems;
+        private readonly LoginStateService _loginStateService;
 
-        [ObservableProperty]
-        public string tab1;
-
-        private readonly string apitoken = "5b9f7f95a73c1b8270ef7cbe664324aac2e9f981f906ad366c64f2b107c90be7";
+        public DashboardView(LoginStateService loginStateService)
+        {
+            _loginStateService = loginStateService;
+        }
 
 
         public DashboardView() { }
@@ -79,41 +79,26 @@ namespace SimplePOSHybrid.Data
         public RestRequest Res()
         {
             GlobalUsings link = new();
+
+            apitoken = _loginStateService.GetToken();
+
             try
             {
 
-                GetPartnerItemListToUploadReqModel reqmodel = new()
+                GetCatgryReqModel reqmodel = new()
                 {
-                    RequestId = "7ee53650-37b8-464c-90e9-85d89f8ab12a",
-                    IntegrationId = "7ee53650-37b8-464c-90e9-85d89f8ab12a",
-                    Apikey = "5b9f7f95a73c1b8270ef7cbe664324aac2e9f981f906ad366c64f2b107c90be7"
-                };
-
-                UID te = new()
-                {
-                    UserKey = 301848,
                     CompanyKey = 51,
-                    LocationKey = 80916,
-                    EnvironmentName = "Live",
-                    IsUrbanPiper = false
+                    CategoryKey = 1
                 };
 
-                reqmodel.UID = te;
 
-                Company co = new()
-                {
-                    CompanyId = 51
-                };
-                reqmodel.company = co;
-
-
-                var request = new RestRequest(link.apilinkpub + "api/PartnerMenu/GetPartnerItemListToUpload").AddJsonBody(reqmodel);
+                var request = new RestRequest(link.apilinkpub + "api/Codebase/getCategories").AddJsonBody(reqmodel);
                 request.Method = Method.Post;
 
                 request.AddHeader("Accept", "application/json");
                 request.AddHeader("Authorization", "Bearer " + apitoken);
                 request.AddHeader("Content-Type", "application/json");
-                request.AddHeader("TImeStamp", "125635753");
+
 
                 return request;
 
